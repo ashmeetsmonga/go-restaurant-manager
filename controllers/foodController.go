51e40dda00b7746b988b99246aea80e4e3cd8c19
ashmeetsmonga <ashmeetsmonga@gmail.com>
2025-02-01 11:31:10 +0530

    first commit

---
+++ controllers/foodController.go
@@ -0,0 +1,177 @@
+package controller
+
+import (
+	"context"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+var validate = validator.New()
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Second)
+		defer cancel()
+
+		var allFood []models.Food
+
+		recordsPerPage, err := strconv.Atoi(c.Query("recordsPerPage"))
+		if err != nil || recordsPerPage < 1 {
+			recordsPerPage = 10
+		}
+
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 10
+		}
+
+		startIndex := (page - 1) * recordsPerPage
+		startIndex, err = strconv.Atoi(c.Query("startIndex"))
+
+		matchStage := bson.D{{Key: "$match", Value: bson.D{{}}}}
+		groupStage := bson.D{{Key: "$group", Value: bson.D{{Key: "_id", Value: bson.D{{Key: "_id", Value: "null"}}}, {Key: "total_count", Value: bson.D{{Key: "$sum", Value: 1}}}, {Key: "data", Value: bson.D{{Key: "$push", Value: "$$ROOT"}}}}}}
+		projectStage := bson.D{
+			{
+				Key: "$project", Value: bson.D{
+					{Key: "_id", Value: 0},
+					{Key: "total_count", Value: 1},
+					{Key: "food_items", Value: bson.D{{Key: "$slice", Value: []interface{}{"$data", startIndex, recordsPerPage}}}},
+				}}}
+
+		result, err := foodCollection.Aggregate(ctx, mongo.Pipeline{
+			matchStage, groupStage, projectStage})
+
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": "Unable to fetch all foods"})
+			return
+		}
+
+		if err := result.All(ctx, &allFood); err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(http.StatusOK, allFood)
+
+	}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Second)
+		defer cancel()
+
+		foodId := c.Param("food_id")
+		var food models.Food
+
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error in fetching food"})
+		}
+
+		c.JSON(http.StatusOK, food)
+	}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var food models.Food
+		var menu models.Menu
+		ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Second)
+		defer cancel()
+
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if validationErr := validate.Struct(food);validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": food.Menu_id}).Decode(&menu)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Menu not found"})
+			return
+		}
+
+		food.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		food.ID = primitive.NewObjectID()
+		food.Food_id = food.ID.Hex()
+
+		result, err := foodCollection.InsertOne(ctx, food)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add food"})
+			return
+		}
+
+		c.JSON(http.StatusOK, result)
+	}
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		foodId := c.Param("food_id") 
+        ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+        defer cancel()
+
+        var food models.Food
+        if err := c.BindJSON(&food); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+
+        validationErr := validate.Struct(food)
+        if validationErr != nil {
+            c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+            return
+        }
+
+        update := bson.M{
+            "$set": bson.M{},
+        }
+
+        if food.Name != nil {
+            update["$set"].(bson.M)["name"] = food.Name
+        }
+        if food.Price != nil {
+            update["$set"].(bson.M)["price"] = food.Price
+        }
+        if food.Food_image != nil {
+            update["$set"].(bson.M)["food_image"] = food.Food_image
+        }
+        if food.Menu_id != nil {
+            update["$set"].(bson.M)["menu_id"] = food.Menu_id
+        }
+
+        update["$set"].(bson.M)["updated_at"] = time.Now()
+
+        filter := bson.M{"food_id": foodId}
+
+        result, err := foodCollection.UpdateOne(ctx, filter, update)
+        if err != nil {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "Food update failed"})
+            return
+        }
+
+        if result.MatchedCount == 0 {
+            c.JSON(http.StatusNotFound, gin.H{"error": "Food not found"})
+            return
+        }
+
+        c.JSON(http.StatusOK, gin.H{"message": "Food updated successfully"})
+	}
+}
---
+++ database/databaseConnection.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDb := os.Getenv("MONGO_URI")
+
+	fmt.Println("Connecting to MongoDB Atlas...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal("Error connecting to MongoDB Atlas:", err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not ping MongoDB Atlas:", err)
+	}
+
+	fmt.Println("Connected to MongoDB Atlas!")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	return client.Database("restaurant").Collection(collectionName)
+}
