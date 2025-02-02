cf4838fa4fcec3b3367dd8bff95aa9fba6f479a9
ashmeetsmonga <ashmeetsmonga@gmail.com>
2025-02-02 22:42:53 +0530

    Add GetAllTables

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,9 +1,14 @@
 package controller
 
 import (
+	"context"
 	"golang-restaurant-management/database"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -12,7 +17,18 @@ var tableCollection *mongo.Collection = database.OpenCollection(database.Client,
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing table items"})
+		}
+		var allTables []bson.M
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allTables)
 	}
 }
 
