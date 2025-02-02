6bdf06e65a30b8ac48f61e5fae61193af670ca1d
ashmeetsmonga <ashmeetsmonga@gmail.com>
2025-02-02 14:46:05 +0530

    Add getAllOrderItems

--- controllers/orderItemController.go
+++ controllers/orderItemController.go
@@ -1,10 +1,44 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type OrderItemPack struct {
+	Table_id    *string
+	Order_items []models.OrderItem
+}
+
+var orderItemCollection *mongo.Collection = database.OpenCollection(database.Client, "orderItem")
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Second)
+		defer cancel()
 
+		var allOrderItems []models.OrderItem 
+		
+		results, err := orderCollection.Find(ctx, bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Unable to fetch orderitems"})
+			return
+		}
+
+		if err := results.All(ctx, &allOrderItems); err != nil {
+			log.Fatal("Unable to parse order items")
+			return
+		}
+
+		c.JSON(http.StatusOK, allOrderItems)
 	}
 }
 
