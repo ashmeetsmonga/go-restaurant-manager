77b28a64442e787c6824c3ad2f288d725def5f6e
ashmeetsmonga <ashmeetsmonga@gmail.com>
2025-02-03 15:11:38 +0530

    Add GetUser

--- controllers/userController.go
+++ controllers/userController.go
@@ -37,7 +37,19 @@ func GetUsers() gin.HandlerFunc {
 
 func GetUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		
+		ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Second)
+		defer cancel()
+
+		userId := c.Param("user_id")
+		var user models.User
+
+		 err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		 if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to fetch user"})
+			return
+		 }
+
+		 c.JSON(http.StatusOK, user)
 	}
 }
 
