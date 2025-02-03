d1fbf9171fa1965bb9c5eea42549231fdeb01b92
ashmeetsmonga <ashmeetsmonga@gmail.com>
2025-02-03 19:00:43 +0530

    Add AuthMiddleware

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,9 +1,40 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	helper "golang-restaurant-management/helpers"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No valid Authorization header provided"})
+			c.Abort()
+			return
+		}
+		clientToken := strings.TrimPrefix(authHeader, "Bearer ")
+		if clientToken == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No Auth Token provided"})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusForbidden, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.First_name)
+		c.Set("last_name", claims.Last_name)
+		c.Set("uid", claims.Uid)
+
+		c.Next()
 	}
 }
